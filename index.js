6ddc56c220cc572821b69db01c9cbf5b6b9f2304
Max Scheff <mxscheffler4@gmail.com>
2023-08-10 17:38:59 +0200

    Slash commands registriert

--- index.js
+++ index.js
@@ -1,27 +1,44 @@
 import { config } from 'dotenv';
-import { Client, GatewayIntentBits, messageLink } from 'discord.js';
+import { Client, GatewayIntentBits, Routes } from 'discord.js';
+import { REST } from '@discordjs/rest';
 
 config();
+const TOKEN = process.env.TOKEN;
+const CLIENT_ID = process.env.CLIENT_ID;
+const GUILD_ID = process.env.GUILD_ID;
 
 //man braucht die Intents damit Discord bestimmte Dinge an den Bot schickt wie zum Beispiel messages
 const client = new Client({
     intents: [
         GatewayIntentBits.Guilds,
         GatewayIntentBits.GuildMessages,
-        GatewayIntentBits.MessageContent,        
+        GatewayIntentBits.MessageContent,
     ],
 });
 
-const TOKEN = process.env.TOKEN;
-client.login(TOKEN);
+const rest = new REST({ version: '10' }).setToken(TOKEN);
 
 client.on('ready', () => {
     console.log(`${client.user.tag} has logged in!`);
 });
 
-client.on('messageCreate', (message) => {
-    console.log(message.content);
-    if (message.author.username != client.user.username) {
-        message.channel.send(`Hallo ${message.author.username}!`);
+async function main() {
+    const commands = [
+        {
+            name: 'tutorialhelp',
+            description: 'help'
+        },
+    ];
+
+    try {
+        console.log('Started refreshing application (/) commands.');
+        await rest.put(Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID), {
+            body: commands,
+        });
+        client.login(TOKEN);
+    } catch (err) { 
+        console.log(err);
     }
-});
+}
+
+main();
