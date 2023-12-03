4c80ad9f8fef974f654e3ed2d71d41eb9e7cba04
Max Scheff <mxscheffler4@gmail.com>
2023-12-03 09:34:22 +0100

    projekt wie bei tutorial gestartet

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import { config } from 'dotenv';
 import { Client, GatewayIntentBits, Routes } from 'discord.js';
 import { REST } from '@discordjs/rest';
+import { DisTube } from 'distube';
 
 config();
 const TOKEN = process.env.TOKEN;
@@ -12,9 +13,17 @@ const client = new Client({
     intents: [
         GatewayIntentBits.Guilds,
         GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.GuildVoiceStates,
         GatewayIntentBits.MessageContent,
     ],
 });
+
+client.DisTube = new DisTube(client, {
+    leaveOnStop: false,
+    emitNewSongOnly: true,
+    emitAddSongWhenCreatingQueue: false,
+    emitAddListWhenCreatingQueue: false,
+})
 
 const rest = new REST({ version: '10' }).setToken(TOKEN);
 
@@ -22,12 +31,27 @@ client.on('ready', () => {
     console.log(`${client.user.tag} has logged in!`);
 });
 
+client.on('interactionCreate', (interaction) => {
+    if (interaction.commandName == 'play') {
+        interaction.reply({ content: 'Hey there!!' + interaction.options });
+
+    }
+    if (interaction.commandName == 'stop') {
+        interaction.reply({ content: 'stopping!!' + interaction.options });
+
+    }
+})
+
 async function main() {
     const commands = [
         {
-            name: 'tutorialhelp',
-            description: 'help'
+            name: 'play',
+            description: 'play a song!'
         },
+        {
+            name: 'stop',
+            description: 'stop the current player!'
+        }
     ];
 
     try {
